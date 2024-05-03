1625efc18401d243e74231f574bf5238e166c5df
Lorenzo Setale <lorenzo@setale.me>
2024-05-03 14:09:57 +0200

    enables ipv6 and disables fail2ban on tailscale
    Signed-off-by: Lorenzo Setale <lorenzo@setale.me>

--- nixos/common.nix
+++ nixos/common.nix
@@ -29,6 +29,7 @@
   i18n.defaultLocale = "en_US.UTF-8";
 
   networking.networkmanager.enable = true;
+  networking.enableIPv6 = true;
 
   # Sets DNS to Cloudflare
   networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
@@ -38,7 +39,17 @@
   networking.firewall.enable = true;
   networking.firewall.allowPing = true;
   networking.firewall.logRefusedConnections = true;
-  services.fail2ban.enable = true;
+
+  services.fail2ban = {
+    enable = true;
+    maxretry = 3;
+    ignoreIP = ["100.64.0.0/10"];
+    bantime = "5m";
+    bantime-increment { 
+      enable = true;
+      maxtime = "24h";
+    };
+  };
 
   # Allow Mosh and SSH
   networking.firewall.allowedTCPPorts = with networking; [ 22 ];
@@ -52,7 +63,7 @@
 
   # Adds tailscale connectivity
   services.tailscale.enable = true;
-  services.tailscale.extraUpFlags = "--ssh";
+  services.tailscale.extraUpFlags = "--ssh --accept-routes";
 
   # At and Cron for scheduling
   services.atd.enable = true;
