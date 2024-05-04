b8fc938fb0d33364c2b758e77081754c25b5ceab
Lorenzo Setale <lorenzo@setale.me>
2024-05-04 15:16:12 +0200

    adds a basic dev shell with latest unstsable packages
    Signed-off-by: Lorenzo Setale <lorenzo@setale.me>

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Lorenzo's HomeLab";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (msystem:
+    let
+      pkgs = import nixpkgs { system = "${msystem}"; config.allowUnfree = true; };
+    in
+    {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          pkgs.coreutils
+          pkgs.coreutils
+          pkgs.jinja2-cli
+          pkgs.python311Packages.jinja2
+          pkgs.python311Packages.jinja2-ansible-filters
+          pkgs.nomad
+          pkgs.consul
+          pkgs.gnumake
+          pkgs.python311Packages.dnspython
+          pkgs.python311Packages.ansible
+          pkgs.jq
+          pkgs.git
+          pkgs.radicle-cli
+        ];
+      };
+    }
+  );
+}
