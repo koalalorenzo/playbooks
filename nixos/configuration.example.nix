353d2127d8fa87775d974cd842f22a69df16e228
Lorenzo Setale <lorenzo@setale.me>
2024-05-28 13:18:27 +0200

    adds missing module to import for networking
    Signed-off-by: Lorenzo Setale <lorenzo@setale.me>

--- nixos/configuration.example.nix
+++ nixos/configuration.example.nix
@@ -3,7 +3,8 @@
   imports = [ 
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
-    
+
+    ./networking.nix
     ./common.nix
     # ./consul.nix
     # ./nomad.nix
