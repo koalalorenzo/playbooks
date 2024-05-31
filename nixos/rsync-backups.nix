2a5f2995c6e787a5d76baa1b339e893433dfa094
Lorenzo Setale <lorenzo@setale.me>
2024-05-31 11:43:47 +0200

    adds vrising systemd rsync backup
    Signed-off-by: Lorenzo Setale <lorenzo@setale.me>

---
+++ nixos/rsync-backups.nix
@@ -0,0 +1,27 @@
+{ config, lib, pkgs, networking, ... }:
+{
+  # Backup service (Push to Storage0 via local network)
+  systemd.services.vrising-backup = {
+    enable = true;
+    serviceConfig.Type = "oneshot";
+    serviceConfig.User = "koalalorenzo";
+    path = with pkgs; [ 
+      bash
+      rsync
+      openssh
+    ];
+    script = ''
+      rsync -avzh --exclude "*.log" --progress /opt/vrising 192.168.197.5:/main/share/
+    '';
+  };
+
+  systemd.timers.vrising-backup = {
+    enable = true;
+    wantedBy = [ "timers.target" ];
+    partOf = [ "vrising-backup.service" ];
+    timerConfig = {
+      OnCalendar = "*-*-* *:58:00";
+      Unit = "vrising-backup.service";
+    };
+  };
+}
