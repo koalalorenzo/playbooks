655e0c211852ef2ecddfea55d04483605272f754
Lorenzo Setale <lorenzo@setale.me>
2024-06-04 16:31:56 +0200

    adds example for extra setup based on the hardware
    Signed-off-by: Lorenzo Setale <lorenzo@setale.me>

--- nixos/configuration.example.nix
+++ nixos/configuration.example.nix
@@ -3,6 +3,9 @@
   imports = [ 
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    
+    # ./macbookpro.nix
+    # ./rpi4.nix
 
     ./networking.nix
     ./common.nix
