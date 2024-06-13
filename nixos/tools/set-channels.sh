4c064e2fbf693509ef8ea51fe185cdcccb77ee4b
Lorenzo Setale <lorenzo@setale.me>
2024-06-13 13:11:27 +0200

    adds /moves scripts to their own tool directory for nixos
    Signed-off-by: Lorenzo Setale <lorenzo@setale.me>

---
+++ nixos/tools/set-channels.sh
@@ -0,0 +1,5 @@
+sudo nix-channel --add https://github.com/NixOS/nixos-hardware/archive/master.tar.gz nixos-hardware
+sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-24.05.tar.gz home-manager
+sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
+sudo nix-channel --add https://nixos.org/channels/nixos-24.05 nixos
+sudo nix-channel --update
